0aa953aa4e0b80caa2ffb2b9e5894959cd875eee
Calum <calum@appoly.co.uk>
2020-03-13 15:02:53 +0000

    remove print_r

--- resources/views/mail-web/index.blade.php
+++ resources/views/mail-web/index.blade.php
@@ -1,8 +1,6 @@
 @extends('mail-web::layouts.app')
 @section('content')
 
-{{print_r(config('MailWeb.MAILWEB_TOOLBAR'))}}
-
 <mail-web :toolbar={{ json_encode(config('MailWeb.MAILWEB_TOOLBAR')) }}></mail-web>
 
 @endsection
