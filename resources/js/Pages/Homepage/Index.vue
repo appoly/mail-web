cd2d3c190fdb3750bc05f22170a4bc2f37d4b060
Calum <calum@appoly.co.uk>
2020-03-13 12:46:39 +0000

    add icons and fix load emails

--- resources/js/Pages/Homepage/Index.vue
+++ resources/js/Pages/Homepage/Index.vue
@@ -1,55 +1,67 @@
 <template>
-    <div class="mx-5 w-full">
-        <div class="flex flex-wrap">
-            <div class="header w-full my-5">
-                <p class="text-4xl">Mail Web</p>
-                <hr />
-            </div>
+	<!-- eslint-disable vue/no-parsing-error -->
+	<div class="mx-5 w-full">
+		<div class="flex flex-wrap">
+			<div class="header w-full my-5">
+				<p class="text-4xl">
+					Mail Web
+				</p>
+				<hr>
+			</div>
 
-            <div class="w-1/4">
-                <div v-for="email in emails" :key="email.id">
-                    <div
-                        :class="['card mx-2 mb-2 cursor-pointer', { 'bg-gray-200': email === selectedEmail }]"
-                        @click="selectedEmail = email"
-                    >
-                        <div class="card-header p-2">
-                            <span class="block">From <{{ getFromEmailAddress(email.from_email) }}></span>
-                            <span class="block">To <{{ email.to_email }}></span>
-                            <span class="block font-semibold">{{ email.subject }}</span>
-                            <span class="block">Sent: {{ parseDate(email.created_at) }}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="w-3/4 h-screen" v-if="selectedEmail !== null">
-                <!-- <div class="flex flex-wrap mb-2"> -->
-                <div class="card w-full mx-5 mb-3">
-                    <button :class="[{ 'text-gray-300': view === 'xl' }, 'btn']" @click="view = 'xl'">XL</button>
-                    <button :class="[{ 'text-gray-300': view === 'md' }, 'btn']" @click="view = 'md'">MD</button>
-                    <button :class="[{ 'text-gray-300': view === 'sm' }, 'btn']" @click="view = 'sm'">SM</button>
-                    <button :class="[{ 'text-gray-300': view === 'html' }, 'btn']" @click="view = 'html'">
-                        HTML Source
-                    </button>
-                    <button :class="[{ 'text-gray-300': view === 'markdown' }, 'btn']" @click="view = 'markdown'">
-                        Markdown
-                    </button>
-                </div>
-                <!-- </div> -->
-                <div class="card h-full mx-5 flex justify-center bg-gray-200">
-                    <iframe
-                        v-if="view === 'xl' || view === 'md' || view === 'sm'"
-                        :class="[widthClass, 'h-screen']"
-                        :srcdoc="selectedEmail.body"
-                        frameborder="0"
-                    ></iframe>
-                    <pre class="text-sm" v-else-if="view === 'html'"><code>{{ selectedEmail.body }}</code></pre>
-                    <pre class="text-sm" v-else>
+			<div class="w-1/4">
+				<div v-for="email in emails" :key="email.id">
+					<div
+						:class="['card mx-2 mb-2 cursor-pointer', { 'bg-gray-200': email === selectedEmail }]"
+						@click="selectedEmail = email"
+					>
+						<div class="card-header p-2">
+							<span class="block">From < {{ getFromEmailAddress(email.from_email) }} ></span>
+							<span class="block">To < {{ email.to_email }} ></span>
+							<span class="block font-semibold">{{ email.subject }}</span>
+							<span class="block">Sent: {{ parseDate(email.created_at) }}</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div v-if="selectedEmail !== null" class="w-3/4 h-screen">
+				<!-- <div class="flex flex-wrap mb-2"> -->
+				<div class="card w-full mx-5 mb-3">
+					<button :class="[{ 'text-gray-300': view === 'xl' }, 'btn']" @click="view = 'xl'">
+						<!-- <img src="/vendor/mailweb/icons/Monitor.svg" width="35"> -->
+						LG
+					</button>
+					<button :class="[{ 'text-gray-300': view === 'md' }, 'btn']" @click="view = 'md'">
+						<!-- <img src="/vendor/mailweb/icons/Laptop.svg" width="35"> -->
+						MD
+					</button>
+					<button :class="[{ 'text-gray-300': view === 'sm' }, 'btn']" @click="view = 'sm'">
+						<!-- <img src="/vendor/mailweb/icons/iPhone.svg" width="35"> -->
+						SM
+					</button>
+					<button :class="[{ 'text-gray-300': view === 'html' }, 'btn']" @click="view = 'html'">
+						HTML Source
+					</button>
+					<button :class="[{ 'text-gray-300': view === 'markdown' }, 'btn']" @click="view = 'markdown'">
+						Markdown
+					</button>
+				</div>
+				<!-- </div> -->
+				<div class="card h-full mx-5 flex justify-center bg-gray-200">
+					<iframe
+						v-if="view === 'xl' || view === 'md' || view === 'sm'"
+						:class="[widthClass, 'h-screen border-2']"
+						:srcdoc="selectedEmail.body"
+						frameborder="0"
+					/>
+					<pre v-else-if="view === 'html'" class="text-sm"><code>{{ selectedEmail.body }}</code></pre>
+					<pre v-else class="text-sm">
                         {{ markdown }}
                     </pre>
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -58,17 +70,22 @@ import turndown from 'turndown';
 import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            selectedEmail: null,
+            view: 'xl',
+            emails: [],
+        };
+    },
     computed: {
         widthClass() {
             switch (this.view) {
-                case 'md':
-                    return 'w-1/2';
-                    break;
-                case 'sm':
-                    return 'w-1/4';
-                    break;
-                default:
-                    return 'w-full';
+            case 'md':
+                return 'medium';
+            case 'sm':
+                return 'small';
+            default:
+                return 'large';
             }
         },
         markdown() {
@@ -79,14 +96,8 @@ export default {
             return '';
         },
     },
-    data() {
-        return {
-            selectedEmail: null,
-            view: 'xl',
-            emails: [],
-        };
-    },
     mounted() {
+        this.getEmails();
         window.setInterval(() => {
             this.getEmails();
         }, 5000);
@@ -116,5 +127,17 @@ pre {
     padding: 1em;
     overflow: auto;
     margin: 0.5em 0;
+}
+.small{
+    width: 375px;
+    height: 812px;
+}
+.medium {
+    width: 1280px;
+    height: 800px;
+}
+.large {
+    width: 1920px;
+    height: 1028px;
 }
 </style>
