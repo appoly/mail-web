0952a758b8cec252d0d17d11069f36c1edf6c95b
Calum Chamberlain <34173688+CalumChamberlain@users.noreply.github.com>
2022-08-24 11:19:10 +0100

    auth middleware is required

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/mailweb', 'Appoly\MailWeb\Http\Controllers\MailWebController@index');
-Route::get('/mailweb/emails', 'Appoly\MailWeb\Http\Controllers\MailWebController@get');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/mailweb', 'Appoly\MailWeb\Http\Controllers\MailWebController@index');
+    Route::get('/mailweb/emails', 'Appoly\MailWeb\Http\Controllers\MailWebController@get');
+});
