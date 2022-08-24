3c5e1950cc872f959ba75f94d8b55189e860688f
Calum Chamberlain <34173688+CalumChamberlain@users.noreply.github.com>
2022-08-24 11:13:10 +0100

    remove middleware as a gate is being used

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['web', 'auth']], function () {
-    Route::get('/mailweb', 'Appoly\MailWeb\Http\Controllers\MailWebController@index');
-    Route::get('/mailweb/emails', 'Appoly\MailWeb\Http\Controllers\MailWebController@get');
-});
+Route::get('/mailweb', 'Appoly\MailWeb\Http\Controllers\MailWebController@index');
+Route::get('/mailweb/emails', 'Appoly\MailWeb\Http\Controllers\MailWebController@get');
