53e8e0c9adc42e17a28130238fc878c45ec2c264
Nathan James <nathanjms@outlook.com>
2023-02-24 10:36:21 +0000

    Push test to see if this is updating

--- resources/views/mail-web/index.blade.php
+++ resources/views/mail-web/index.blade.php
@@ -1,6 +1,6 @@
 @extends('mail-web::layouts.app')
 @section('content')
-
+<h1>Test that this is updating on dev</h1>
 <mail-web :toolbar={{ json_encode(config('MailWeb.MAILWEB_TOOLBAR')) }}></mail-web>
 
 @endsection
