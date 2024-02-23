717e858acd9bff52b584c9d40396cd52d70b015a
Calum Chamberlain <calum@appoly.co.uk>
2024-02-23 15:34:34 +0000

    toolbar classes

--- resources/views/components/toolbar-button.blade.php
+++ resources/views/components/toolbar-button.blade.php
@@ -2,6 +2,6 @@
     'type' => 'button',
     'attributes' => [],
 ])
-<button {{ $attributes->except('class') }} type="{{ $type }}" {{ $attributes->only('class')->merge(['class' => "hover:bg-blue-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-[background-color] ease-in-out duration-300"]) }}>
+<button {{ $attributes->except('class') }} type="{{ $type }}" {{ $attributes->only('class')->merge(['class' => "text-gray-900 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-[background-color] ease-in-out duration-300 dark:text-white"]) }}>
     {{ $slot }}
 </button>
