efb3c727d666798aee119a1741547876fdcee99c
Calum Chamberlain <calum@appoly.co.uk>
2024-03-01 11:04:24 +0000

    unread pulse

--- resources/views/livewire/email-list-view.blade.php
+++ resources/views/livewire/email-list-view.blade.php
@@ -9,52 +9,44 @@
     <div class="overflow-y-auto scrollbar-hide max-h-[calc(100vh-200px)]">
         <div class="flex flex-col gap-4">
             @foreach ($emails as $email)
-                <div class="px-4 pt-6 pb-8 bg-white rounded-lg shadow cursor-pointer dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
+                <div class="bg-white rounded-lg shadow cursor-pointer dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
                     wire:click="showEmail('{{ $email->id }}')">
-                    <div class="flex gap-4">
-                        <div class="flex-shrink-0">
-                            <img src="https://ui-avatars.com/api/?name={{ $email->from[0]['name'] }}"
-                                class="rounded-full w-[40px]">
-                        </div>
-                        <div class="flex flex-col gap-1">
-                            <div class="flex">
-                                <h1 class="font-bold text-md dark:text-gray-200">
-                                    @foreach ($email->from as $from)
-                                        {{ $from['name'] }} <span
-                                            class="text-sm text-gray-700 dark:text-gray-300">({{ $from['address'] }})</span>
-                                    @endforeach
-                                </h1>
-                                {{-- unread mark --}}
-                                <div class="ml-auto text-gray-500 dark:text-gray-300">
-                                    @if (!$email->read)
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25"
-                                            stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail">
-                                            <rect width="20" height="16" x="2" y="4" rx="2" />
-                                            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-                                        </svg>
-                                    @else
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="lucide lucide-mail-open">
-                                            <path
-                                                d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z" />
-                                            <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10" />
-                                        </svg>
-                                    @endif
-                                </div>
+                    <div class="flex justify-end pt-2 pr-2">
+                        @if (!$email->read)
+                            <span class="relative flex w-3 h-3">
+                                <span
+                                    class="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-sky-400"></span>
+                                <span class="relative inline-flex w-3 h-3 rounded-full bg-sky-500"></span>
+                            </span>
+                        @endif
+                    </div>
+                    <div class="px-4 pt-2 pb-8 content">
+
+                        <div class="flex gap-4">
+                            <div class="flex-shrink-0">
+                                <img src="https://ui-avatars.com/api/?name={{ $email->from[0]['name'] }}"
+                                    class="rounded-full w-[40px]">
                             </div>
-                            <p class="text-sm text-gray-700 dark:text-gray-300">
-                                {{ $email->subject }}
-                            </p>
-                            <p class="text-xs text-gray-700 dark:text-gray-300">
-                                {{ $email->snippet }}
-                            </p>
-                            <div class="flex content-end gap-4">
-                                <p class="ml-auto text-xs text-gray-700 dark:text-gray-300">
-                                    {{ $email->created_at->diffForHumans() }}
+                            <div class="flex flex-col gap-1">
+                                <div class="flex">
+                                    <h1 class="font-bold text-md dark:text-gray-200">
+                                        @foreach ($email->from as $from)
+                                            {{ $from['name'] }} <span
+                                                class="text-sm text-gray-700 dark:text-gray-300">({{ $from['address'] }})</span>
+                                        @endforeach
+                                    </h1>
+                                </div>
+                                <p class="text-sm text-gray-700 dark:text-gray-300">
+                                    {{ $email->subject }}
                                 </p>
+                                <p class="text-xs text-gray-700 dark:text-gray-300">
+                                    {{ $email->snippet }}
+                                </p>
+                                <div class="flex content-end gap-4">
+                                    <p class="ml-auto text-xs text-gray-700 dark:text-gray-300">
+                                        {{ $email->created_at->diffForHumans() }}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
