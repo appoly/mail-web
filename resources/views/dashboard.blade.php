d1c56b2618bd78cd3fcc7392ce42d77b7fc38fbc
Calum Chamberlain <calum@appoly.co.uk>
2024-03-22 10:40:26 +0000

    assets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('mailweb::layouts.app')
 @section('content')
-    <div class="min-h-screen py-6 text-gray-700 bg-slate-50 dark:bg-slate-900 sm:px-8 dark:text-white">
+    <div class="min-h-screen py-8 text-gray-700 bg-slate-50 dark:bg-slate-900 sm:px-8 dark:text-white">
         <div class="header md:mx-auto">
             <h1 class="font-bold text-8xl">
                 Mail Web
