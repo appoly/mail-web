d006b1253830fae189d740e95c2c24c1120797ee
Nathan James <nathanjms@outlook.com>
2024-05-07 14:37:59 +0100

    Reformat the config

--- config/config.php
+++ config/config.php
@@ -13,6 +13,8 @@ return [
     'MAILWEB_ENABLED' => env('MAILWEB_ENABLED', true),
     'MAILWEB_LIMIT' => env('MAILWEB_LIMIT', 20),
     'MAILWEB_SAMPLE_SECTION' => env('MAILWEB_SAMPLE_SECTION', true),
-    'MAILWEB_APP_NAME' => env('MAILWEB_APP_NAME') ?? env('APP_NAME'),
-    'MAILWEB_APP_URL' => env('MAILWEB_APP_URL') ?? env('APP_URL'),
+    'MAILWEB_RETURN' => [
+        'APP_NAME' => env('MAILWEB_RETURN_APP_NAME'),
+        'APP_URL' => env('MAILWEB_RETURN_APP_URL'),
+    ],
 ];
