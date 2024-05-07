249efb704fa87866e0173be2f02b28875675af49
Nathan James <nathanjms@outlook.com>
2024-05-07 14:20:56 +0100

    Allow customisable APP Name and App URL, separate from app config.

--- config/config.php
+++ config/config.php
@@ -13,4 +13,6 @@ return [
     'MAILWEB_ENABLED' => env('MAILWEB_ENABLED', true),
     'MAILWEB_LIMIT' => env('MAILWEB_LIMIT', 20),
     'MAILWEB_SAMPLE_SECTION' => env('MAILWEB_SAMPLE_SECTION', true),
+    'MAILWEB_APP_NAME' => env('MAILWEB_APP_NAME') ?? env('APP_NAME'),
+    'MAILWEB_APP_URL' => env('MAILWEB_APP_URL') ?? env('APP_URL'),
 ];
