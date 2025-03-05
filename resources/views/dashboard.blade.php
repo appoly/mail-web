e8df739f84fbbc2782e12dfa56e6d6b148a6c0ad
Calum Chamberlain <calum@appoly.co.uk>
2025-03-05 15:46:00 +0000

    fix displaying vue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LaraTrack Dashboard</title>
+    <title>Mail Web Dashboard</title>
     
     @php
-        $manifestPath = public_path('vendor/laratrack/.vite/manifest.json');
+        $manifestPath = public_path('vendor/mailweb/manifest.json');
         $manifest = file_exists($manifestPath) 
             ? json_decode(file_get_contents($manifestPath), true) 
             : [];
-        $jsPath = $manifest['resources/js/laratrack.js']['file'] ?? null;
-        $cssPath = $manifest['resources/js/laratrack.js']['css'][0] ?? null;
+        $jsPath = $manifest['resources/js/mailweb.js']['file'] ?? null;
+        $cssPath = $manifest['resources/js/mailweb.js']['css'][0] ?? null;
     @endphp
     
     @if($cssPath)
-        <link rel="stylesheet" href="{{ asset('vendor/laratrack/' . $cssPath) }}">
+        <link rel="stylesheet" href="{{ asset('vendor/mailweb/' . $cssPath) }}">
     @endif
     
     @if($jsPath)
-        <script type="module" src="{{ asset('vendor/laratrack/' . $jsPath) }}"></script>
+        <script type="module" src="{{ asset('vendor/mailweb/' . $jsPath) }}"></script>
     @endif
 </head>
 <body>
     <div id="app">
-        <laratrack-dashboard></laratrack-dashboard>
+        <mailweb-dashboard></mailweb-dashboard>
     </div>
 </body>
 </html>
