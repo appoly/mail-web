6552b53b4738b9bf2d275b0224ff5cbdb5f67ea3
Calum Chamberlain <calum@appoly.co.uk>
2025-03-14 12:11:24 +0000

    🤦‍♂️
    Co-Authored-By: John Wedgbury <7074044+johnwedgbury@users.noreply.github.com>

--- resources/js/components/SidebarContent.vue
+++ resources/js/components/SidebarContent.vue
@@ -198,7 +198,7 @@ onMounted(() => {
         watch(paginationTriggered, (triggered) => {
             if (triggered && isPolling.value) {
                 stopPolling()
-                toast.success('Auto-refresh has been disabled while you\'re browsing. You can re-enable it in the bottom right corner')
+                toast.success('Auto-refresh has been disabled while you\'re browsing. You can re-enable it in the bottom left corner')
             }
         })
     }
